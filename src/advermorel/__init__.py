e4793f397bd2013f8f587e16a8e3c2984368e45e
Salomon Hotegni <91737217+salomonhotegni@users.noreply.github.com>
2025-06-29 07:11:29 +0200

    Create __init__.py

---
+++ src/advermorel/__init__.py
@@ -0,0 +1,9 @@
+"""Top-level package for MOREL."""
+
+__author__ = """Sedjro Salomon Hotegni"""
+__email__ = 'salomon.hotegni@tu-dortmund.de'
+__version__ = '0.1.1'
+
+from .advermorel import MOREL
+from .losses import morel_loss, mart_loss,  trades_loss, loat_loss
+from .models import morelnet
