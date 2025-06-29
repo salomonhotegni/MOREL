6f7e5e870da6baedd4f087f166eeee4d79cdecba
Salomon Hotegni <91737217+salomonhotegni@users.noreply.github.com>
2025-06-30 00:00:02 +0200

    Update __init__.py

--- src/advermorel/__init__.py
+++ src/advermorel/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Sedjro Salomon Hotegni"""
 __email__ = 'salomon.hotegni@tu-dortmund.de'
-__version__ = '0.1.4'
+__version__ = '0.1.6'
 
 from .advermorel import MOREL
 from .losses import morel_loss, mart_loss,  trades_loss, loat_loss
